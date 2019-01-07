259f318065893610c2b8776bccc3648e71c9a37a
cc123king <287274779@qq.com>
2019-01-07 16:55:16 +0800

    缓存

---
+++ tu.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
+if __name__=='__main__':
+    x=np.linspace(-1,25,30)
+    y=2*x+1
+    plt.plot(x,y)
+
+    plt.show()
+    plt.savefig('static/picture/flash.jpg')
