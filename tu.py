4322d9e37dc1daeea3642d31e1e92d33bd86f49b
cc123king <287274779@qq.com>
2019-01-12 21:20:39 +0800

    try animation

--- tu.py
+++ tu.py
@@ -1,9 +1,19 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import animation
 if __name__=='__main__':
-    x=np.linspace(-1,25,30)
-    y=2*x+1
-    plt.plot(x,y)
-    plt.savefig('static/picture/flash.jpg')
-    plt.show()
+    fig,ax=plt.subplots()
+    x=np.arange(0,2*np.pi,0.01)
+    line, =ax.plot(x,np.sin(x))
 
+    def update(i):
+        line.set_ydata(np.sin(x+i/10))
+        return line,
+    
+    def init():
+        line.set_ydata(np.sin(x))
+        #print(type(line,))
+        return line,
+    print(type(init()))
+    ani=animation.FuncAnimation(fig=fig,func=update,init_func=init,interval=10,blit=False,frames=200)
+    plt.show()
